82d3e8ca47809b942ece69a6365a36489e1ca226
Enes UTKU <ensutku@hotmail.com>
2024-04-28 11:09:46 +0300

    Initial Commit

---
+++ ExpandableProfileAnimation/ExpandableProfileAnimationApp.swift
@@ -0,0 +1,12 @@
+// Created by Enes UTKU
+
+import SwiftUI
+
+@main
+struct ExpandableProfileAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
